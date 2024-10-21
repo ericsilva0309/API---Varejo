ab8fada05b8e8ba151e3319b0127aeae1553db0f
eric.s <ericcarvalhosilva03@gmail.com>
2024-10-21 18:44:32 -0300

    resolvendo erro do h2

--- varejo/src/main/java/com/varejo/controller/FidelidadeController.java
+++ varejo/src/main/java/com/varejo/controller/FidelidadeController.java
@@ -6,13 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.varejo.entity.ClienteFidelidade;
 import com.varejo.service.FidelidadeService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 @RestController
 @RequestMapping("/fidelidade")
--- varejo/src/main/java/com/varejo/controller/ProdutoController.java
+++ varejo/src/main/java/com/varejo/controller/ProdutoController.java
@@ -4,13 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.varejo.entity.Produto;
 import com.varejo.service.ProdutoService;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/produtos")
